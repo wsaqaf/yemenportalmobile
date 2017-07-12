2461c831156b8da6c5c83a092a807821867d2c32
Pavel Vashkel <p.vashkel@datarockets.com>
2017-07-12 19:21:33 +0300

    Push builds to Fabric [restore]

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,3 @@
-import { AppRegistry, NativeModules } from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-
 AppRegistry.registerComponent('yemenportalmobile', () => App);
-
-NativeModules.RNFabric.initWithCrashlytics();
